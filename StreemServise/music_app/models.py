c8a400c11148b801826d65ad6c95c3281559c454
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-15 11:31:23 +0300

    add music model

--- StreemServise/music_app/models.py
+++ StreemServise/music_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+
+class Music(models.Model):
+    song_title = models.CharField(max_length=100)
+    songwriter = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='image/')
+    file = models.FileField(
+        upload_to='music/',
+        validators=[FileExtensionValidator(allowed_extensions=['mp3'])]
+    )
+    release_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.song_title
