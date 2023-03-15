801425aaf48bb7d30ac0a272235b5d6af9af8945
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-15 14:33:52 +0300

    add single_music_page Views

--- StreemServise/music_app/views.py
+++ StreemServise/music_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import StreamingHttpResponse
 
-# Create your views here.
+from .models import Music
+
+
+def music_list_page(request):
+    template_name = 'music_app/home.html'
+    context = {'music_list': Music.objects.all()}
+    return render(request, template_name, context)
+
+
+def single_music_page(requesr, pk):
+    music = get_object_or_404(Music, id=pk)
+    template_name = 'music_app/music.html'
+    context = {'music': music}
+    return render(requesr, template_name, context)
