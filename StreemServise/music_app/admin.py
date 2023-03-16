6afacf12fe585fc7eb027cc45ae64889957c6c73
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 12:32:56 +0300

    register new model

--- StreemServise/music_app/admin.py
+++ StreemServise/music_app/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 
-from .models import Music
+from .models import Song, SongWriter, Album
 
 
-class MusicAdmin(admin.ModelAdmin):
+class SongWriterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'image')
+    list_display_links = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'album_title', 'song_writer')
+    list_display_links = ('album_title',)
+    empty_value_display = '-пусто-'
+
+
+class SongAdmin(admin.ModelAdmin):
     list_display = ('pk', 'song_title', 'song_writer')
     list_display_links = ('song_title', 'song_writer')
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Music, MusicAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(SongWriter, SongWriterAdmin)
+admin.site.register(Song, SongAdmin)
