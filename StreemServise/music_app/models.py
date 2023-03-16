14d6cf40429ab1969d71c0ec9c97191696f77cc3
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 09:55:53 +0300

    Changed the paths for saving files

--- StreemServise/music_app/models.py
+++ StreemServise/music_app/models.py
@@ -5,12 +5,15 @@ from django.core.validators import FileExtensionValidator
 class Music(models.Model):
     song_title = models.CharField(max_length=100)
     songwriter = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='image/%Y/%m/%d/')
     file = models.FileField(
-        upload_to='music/',
+        upload_to='music/%Y/%m/%d/',
         validators=[FileExtensionValidator(allowed_extensions=['mp3'])]
     )
     release_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.song_title
+
+    class Meta:
+        ordering = ['-release_date']
