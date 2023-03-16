e8152111a528faedbbaddec29b0745bcb94378b2
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 09:57:19 +0300

    add namespace

--- StreemServise/StreemServise/urls.py
+++ StreemServise/StreemServise/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include(('music_app.urls', 'music_app'), namespace='music_app')),
     path('admin/', admin.site.urls),
-    path('', include('music_app.urls')),
 ]
