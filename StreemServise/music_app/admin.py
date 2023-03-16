443a697786823e5187945f55bcf43258fcacae53
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 11:42:37 +0300

    small fixes

--- StreemServise/music_app/admin.py
+++ StreemServise/music_app/admin.py
@@ -4,8 +4,8 @@ from .models import Music
 
 
 class MusicAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'song_title', 'songwriter')
-    list_display_links = ('song_title', 'songwriter')
+    list_display = ('pk', 'song_title', 'song_writer')
+    list_display_links = ('song_title', 'song_writer')
     empty_value_display = '-пусто-'
 
 
