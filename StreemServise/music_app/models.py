66682ec4e0a0de0dcc2c3dc4b10def26254abf35
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 11:42:48 +0300

    add new models

--- StreemServise/music_app/models.py
+++ StreemServise/music_app/models.py
@@ -2,18 +2,40 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 
 
-class Music(models.Model):
+class SongWriter(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='song_writer_image/%Y/%m/%d/',
+                              blank=True,
+                              null=True)
+
+
+class Song(models.Model):
     song_title = models.CharField(max_length=100)
-    songwriter = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='image/%Y/%m/%d/')
+    song_writer = models.ForeignKey(SongWriter,
+                                    on_delete=models.CASCADE,
+                                    related_name='song',
+                                    verbose_name='songwriter',
+                                    max_length=100)
     file = models.FileField(
         upload_to='music/%Y/%m/%d/',
         validators=[FileExtensionValidator(allowed_extensions=['mp3'])]
     )
-    release_date = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey('Album', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.song_title
 
     class Meta:
         ordering = ['-release_date']
+
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=100)
+    song_writer = models.ForeignKey(SongWriter,
+                                    related_name='song_writer',
+                                    on_delete=models.CASCADE)
+    file = models.ForeignKey(Song,
+                             related_name='song',
+                             on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='song_image/%Y/%m/%d/')
+    release_date = models.DateTimeField(auto_now_add=True)
