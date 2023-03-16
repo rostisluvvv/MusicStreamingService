85b7bca2a098b345bf6a5c5013f72670e1278197
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-16 16:49:53 +0300

    change contex date

--- StreemServise/music_app/views.py
+++ StreemServise/music_app/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import StreamingHttpResponse
 
-from .models import Music
+from .models import Song, Album, SongWriter
 
 
 def music_list_page(request):
+    albums = Album.objects.all()
+    context = {
+        'albums': albums,
+    }
     template_name = 'music_app/home.html'
-    context = {'music_list': Music.objects.all()}
     return render(request, template_name, context)
 
 
 def single_music_page(requesr, pk):
-    music = get_object_or_404(Music, id=pk)
+    song = get_object_or_404(Song, id=pk)
+    album = song.albums.last()
     template_name = 'music_app/music_page.html'
-    context = {'music': music}
+    context = {
+        'song': song,
+        'album': album,
+    }
     return render(requesr, template_name, context)
